0627a3681d2ad26919af01250b97e591ad20124b
Michaela Martinez <16543630@mstc.edu>
2023-05-02 16:39:21 -0500

    T8-13 finished functionality of mathGame.html

--- TheFinal/js/MathGame.js
+++ TheFinal/js/MathGame.js
@@ -1,55 +1,91 @@
-var score = 0;
-var round = 1;
+const problemElement = document.querySelector(".problem")
+const ourForm = document.querySelector(".our-form")
+const ourField = document.querySelector(".our-field")
+const pointsNeeded = document.querySelector(".points-needed")
+const mistakesAllowed = document.querySelector(".mistakes-allowed")
+const progressBar = document.querySelector(".progress-inner")
+const endMessage = document.querySelector(".end-message")
+const resetButton = document.querySelector(".reset-button")
 
-function generateProblem(operation) {
-  var num1 = Math.floor(Math.random() * 10) + 1;
-  var num2 = Math.floor(Math.random() * 10) + 1;
-  var problemText = "";
-
-  switch (operation) {
-    case "addition":
-      problemText = num1 + " + " + num2 + " =";
-      return num1 + num2;
-    case "subtraction":
-      problemText = num1 + " - " + num2 + " =";
-      return num1 - num2;
-    case "multiplication":
-      problemText = num1 + " x " + num2 + " =";
-      return num1 * num2;
-    case "division":
-      var product = num1 * num2;
-      problemText = product + " / " + num1 + " =";
-      return num2;
-  }
-
-  document.getElementById("problem").innerHTML = problemText;
+let state = {
+  score: 0,
+  wrongAnswers: 0
 }
 
-function checkAnswer() {
-  var answer = parseInt(document.getElementById("answer").value);
-  var operation = document.getElementById("operation").value;
+function updateProblem() {
+  state.currentProblem = generateProblem()
+  problemElement.innerHTML = `${state.currentProblem.numberOne} ${state.currentProblem.operator} ${state.currentProblem.numberTwo}`
+  ourField.value = ""
+  ourField.focus()
+}
 
-  var correctAnswer = generateProblem(operation);
+updateProblem()
 
-  if (answer === correctAnswer) {
-    score++;
-    document.getElementById("score").innerHTML = score;
-    round++;
-    document.getElementById("round").innerHTML = round;
+function generateNumber(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
+function generateProblem() {
+  return {
+    numberOne: generateNumber(10),
+    numberTwo: generateNumber(10),
+    operator: ['+', '-', 'x'][generateNumber(2)]
+  }
+}
+
+ourForm.addEventListener("submit", handleSubmit)
+
+function handleSubmit(e) {
+  e.preventDefault()
+
+  let correctAnswer
+  const p = state.currentProblem
+  if (p.operator == "+") correctAnswer = p.numberOne + p.numberTwo
+  if (p.operator == "-") correctAnswer = p.numberOne - p.numberTwo
+  if (p.operator == "x") correctAnswer = p.numberOne * p.numberTwo
+
+  if (parseInt(ourField.value, 10) === correctAnswer) {
+    state.score++
+    pointsNeeded.textContent = 10 - state.score
+    updateProblem()
+    renderProgressBar()
   } else {
-    round++;
-    document.getElementById("round").innerHTML = round;
+    state.wrongAnswers++
+    mistakesAllowed.textContent = 2 - state.wrongAnswers
+    problemElement.classList.add("animate-wrong")
+    setTimeout(() => problemElement.classList.remove("animate-wrong"), 451)
   }
-
-  document.getElementById("answer").value = "";
-  document.getElementById("answer").focus();
+  checkLogic()
 }
 
-var operationSelect = document.getElementById("operation");
-operationSelect.addEventListener("change", function() {
-  var operation = operationSelect.value;
+function checkLogic() {
+  // if you won
+  if (state.score === 10) {
+    endMessage.textContent = "Congrats! You won."
+    document.body.classList.add("overlay-is-open")
+    setTimeout(() => resetButton.focus(), 331)
+  }
 
-  // Show the hidden item
-  document.getElementsByClassName("problem-container")
+  // if you lost
+  if (state.wrongAnswers === 3) {
+    endMessage.textContent = "Sorry! You lost."
+    document.body.classList.add("overlay-is-open")
+    setTimeout(() => resetButton.focus(), 331)
+  }
+}
 
-});
+resetButton.addEventListener("click", resetGame)
+
+function resetGame() {
+  document.body.classList.remove("overlay-is-open")
+  updateProblem()
+  state.score = 0
+  state.wrongAnswers = 0
+  pointsNeeded.textContent = 10
+  mistakesAllowed.textContent = 2
+  renderProgressBar()
+}
+
+function renderProgressBar() {
+  progressBar.style.transform = `scaleX(${state.score / 10})`
+}
